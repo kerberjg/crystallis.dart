b9620b498a0815ae733820cbc01adc103263b19c
Jamie Kerber <kerber.jg@gmail.com>
2026-03-09 16:08:12 +0100

    Added documented library directives

--- packages/crystallis_plugin/lib/main.dart
+++ packages/crystallis_plugin/lib/main.dart
@@ -1,3 +1,7 @@
+/// The main entry point for the Crystallis analyzer plugin.
+/// This is where the plugin is registered with the analysis server.
+library;
+
 import 'package:crystallis_plugin/src/plugin.dart';
 
 /// The entry point for the plugin. This is where the plugin is registered with the analysis server.
